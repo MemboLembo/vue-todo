d0b3d453ee712d5ea64cb1ae03662ffefd2bf24b
MemboLembo <1nfinitiv6995@gmail.com>
2020-06-18 10:33:31 +0300

    add, delete list items

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todo App</h1>
+    <AddTodo
+      @add-todo="addTodo"
+    />
+    <hr>
+    <TodoList
+      v-bind:todos="todos"
+      @remove-todo="removeTodo"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoList from "@/components/TodoList";
+import AddTodo from "@/components/AddTodo";
 export default {
   name: 'App',
+  data() {
+    return {
+      todos: [
+        {id: 1, title: 'Buy a bread', completed: false},
+        {id: 2, title: 'Buy a bun', completed: false},
+        {id: 3, title: 'Buy a milk', completed: false}
+      ]
+    };
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter(t => t.id !== id);
+    },
+    addTodo(todo) {
+      this.todos.push(todo)
+    }
+  },
   components: {
-    HelloWorld
+    TodoList,
+    AddTodo
   }
 }
 </script>
