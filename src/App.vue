a04a586b126ebd2f7d3fd11d3c2c13409a1f6243
MemboLembo <1nfinitiv6995@gmail.com>
2020-06-18 11:06:57 +0300

    mounted, fetch

--- src/App.vue
+++ src/App.vue
@@ -19,12 +19,15 @@ export default {
   name: 'App',
   data() {
     return {
-      todos: [
-        {id: 1, title: 'Buy a bread', completed: false},
-        {id: 2, title: 'Buy a bun', completed: false},
-        {id: 3, title: 'Buy a milk', completed: false}
-      ]
+      todos: []
     };
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(json => {
+        this.todos = json
+      })
   },
   methods: {
     removeTodo(id) {
